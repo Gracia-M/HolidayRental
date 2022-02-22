de462b7c9cda06737048e4366f492a87923685f2
Grace Muderevu Musabe <gracemusabe@gmail.com>
2022-02-22 22:56:50 +0100

    Some update en BLL

---
+++ HoliDayRental.BLL/Entities/BienEchange.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HoliDayRental.BLL.Entities
+{
+    public class BienEchange
+    {
+        public int idBien { get; set; }
+        public string titre { get; set; }
+        public string DescCourte { get; set; }
+        public string DescLong { get; set; }
+        public int NombrePerson { get; set; }
+        public string Ville { get; set; }
+        public string Rue { get; set; }
+        public string Numero { get; set; }
+        public string CodePostal { get; set; }
+        public string Photo { get; set; }
+        public bool AssuranceObligatoire { get; set; }
+        public bool isEnabled { get; set; }
+        public DateTime? DisabledDate { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public DateTime DateCreation { get; set; }
+        public int idPays { get; set; }
+        public int idMembre { get; set; }
+        public Pays Pays { get; set; }
+        public Membre Membre { get; set; }
+        
+
+        public BienEchange(int id, string title, string shortDescr, string longDescr, int nrOfPeople, string city, string street, string number, string zipcode, string picture, bool insurance, bool enable, DateTime disable, string latitude, string longitude, DateTime creation, Pays country, Membre member)
+        {
+            idBien = id;
+            titre = title;
+            DescCourte = shortDescr;
+            DescLong = longDescr;
+            NombrePerson = nrOfPeople;
+            Ville = city;
+            Rue = street;
+            Numero = number;
+            CodePostal = zipcode;
+            Photo = picture;
+
+        }
+    }
+
+}
---
+++ HoliDayRental.BLL/Handlers/Mapper.cs
@@ -0,0 +1,57 @@
+﻿using B = HoliDayRental.BLL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using D = HoliDayRental.DAL.Entities;
+
+namespace HoliDayRental.BLL.Handlers
+{
+    public static class Mapper
+    {
+        public static B.BienEchange ToBLL(this D.BienEchange entity)
+        {
+            if (entity == null) return null;
+            return new B.BienEchange(
+                entity.idBien,
+                entity.titre,
+                entity.DescCourte,
+                entity.DescLong,
+                entity.NombrePerson,
+                entity.Pays,
+                entity.Ville,
+                entity.Rue,
+                entity.Numero,
+                entity.CodePostal,
+                entity.Photo,
+                entity.AssuranceObligatoire,
+                entity.isEnabled,
+                entity.DisabledDate,
+                entity.Latitude,
+                entity.Longitude,
+
+                );
+        }
+        public static D.Bien ToBLL(this B.Bien entity)
+        {
+            if (entity == null) return null;
+            return new D.Bien
+            {
+                idBien = entity.idBien,
+                titre = entity.titre,
+                descriptionCourte = entity.descriptionCourte,
+                descriptionLongue = entity.descriptionLongue,
+                Pays = entity.Pays,
+                Ville = entity.Ville,
+                Rue = entity.Rue,
+                Numero = entity.Numero,
+                CodePostal = entity.CodePostal,
+                Photo = entity.Photo,
+                nbrSalleBains = entity.nbrSalleBains,
+                nbrWC = entity.nbrWC,
+                dateDebut = entity.dateDebut,
+                dateFin = entity.dateFin
+            };
+
+        }
+    }
+}
